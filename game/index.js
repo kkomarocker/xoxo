21d06955ffb498433f5310ef92c8aa0635aded25
Marino Demo <demo716@gmail.com>
2018-05-16 12:30:38 -0400

    completed blank board

--- game/index.js
+++ game/index.js
@@ -1,6 +1,26 @@
 import {Map} from 'immutable'
 
-export default function reducer(state, action) {
+let board = Map();
+
+// board.setIn([0,0], '_')
+//      .setIn([0,1], '_')
+//      .setIn([0,2], '_')
+//      .setIn([1,0], '_')
+//      .setIn([1,1], '_')
+//      .setIn([1,2], '_')
+//      .setIn([2,0], '_')
+//      .setIn([2,1], '_')
+//      .setIn([2,2], '_')
+
+
+export default function reducer(state = { board: board}, action) {
   // TODO
+
+
   return state
+}
+
+
+const move = () => {
+
 }
